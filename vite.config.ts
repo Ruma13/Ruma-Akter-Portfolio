6e58ea8a42303c42ea05d8433f7a74c45d815c44
Ruma13 <155447790+Ruma13@users.noreply.github.com>
2026-07-13 18:43:05 +0600

    Add comment for cache clearing in vite.config.ts
    Added a comment to clarify cache clearing in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// force rebuild to clear vite config cache memory
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
